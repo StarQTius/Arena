95e961a71fd5cc0381fa3c0a46a519d8250e8fb7
StarQTius <dussautoirp@gmail.com>
2022-02-10 19:35:21 +0100

    Add cup damping

--- src/cup.cpp
+++ src/cup.cpp
@@ -7,6 +7,7 @@ using namespace units::isq::si::literals;
 namespace {
 
 constexpr auto cup_mass = 200_q_g;
+constexpr auto cup_damping = 100_q_s;
 const auto cup_shape = arena::component::make_circle_shape(3_q_cm);
 
 } // namespace
@@ -20,6 +21,8 @@ entt::entity arena::entity::create(b2World &world, entt::registry &registry, con
 
   auto *body_ptr = world.CreateBody(&body_def);
   body_ptr->CreateFixture(&cup_shape, compute_shape_density(cup_shape, cup_mass).number());
+  body_ptr->SetLinearDamping(box2d_time_t{cup_damping}.number());
+  body_ptr->SetAngularDamping(box2d_time_t{cup_damping}.number());
 
   auto self = registry.create();
   registry.emplace<component::BodyPtr>(self, body_ptr);
